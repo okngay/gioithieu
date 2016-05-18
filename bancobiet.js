d8dc57ad8b95d286a1c89da80848715f4215b7b0
Bùi Khương Duy <okngayvn@gmail.com>
2016-05-18 21:29:14 +0700

    Update bancobiet.js

--- bancobiet.js
+++ bancobiet.js
@@ -1,11 +1,5 @@
 
-  $(document).ready(function() {
- var happyimg = ['khanpha.png','khampha2.png', 'khampha3.png', 'khampha3.png', 'khampha4.png'];
+
+ var happyimg = ['khampha2.png', 'khampha3.png', 'khampha3.png', 'khampha4.png'];
  var img = happyimg[Math.floor(Math.random() * happyimg.length)];
  $("#hinhanh").attr("src", "" + img + "");
-
-
-   
-
-     //-------------- DOM ------------------------
- }); //doc ready
