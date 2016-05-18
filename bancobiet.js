ed2795fb418af3d1c327eb1a73f572c0c604956d
Bùi Khương Duy <okngayvn@gmail.com>
2016-05-18 21:35:16 +0700

    Update bancobiet.js

--- bancobiet.js
+++ bancobiet.js
@@ -2,4 +2,4 @@
 
  var happyimg = ['khampha2.png', 'khampha3.png', 'khampha3.png', 'khampha4.png'];
  var img = happyimg[Math.floor(Math.random() * happyimg.length)];
- $("#hinhanh").attr("src", "" + img + "");
+ document.getElementsByTagName("hinhanh")[0].setAttribute("scr", ""+img+""); 
